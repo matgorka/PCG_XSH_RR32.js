69ca26cbf33673eebe8e21870ec2f0ee96039b8e
Mateusz Górka <mateuszgorka@pm.me>
2024-02-13 01:07:12 +0100

    in progress
    returned values are only slightly off

---
+++ pcg_xsh_rr32.js
@@ -0,0 +1,32 @@
+(g => {
+  const multiplier = 6364136223846793005n,
+        increment  = 1442695040888963407n,
+        rotr       = (x, r) => x >> r | x << ((2n ** 32n - r) & 31n);
+
+  class PCG_XSH_RR32 {
+    state = 5573589319906701683n;
+
+    next() {
+      let x,
+          count;
+
+      //assert(typeof x == "uint64_t")
+      x           = this.state;
+      count       = this.state >> 59n;
+      this.state  = x * multiplier + increment;
+      this.state %= 2n**64n;
+      x          ^= x >> 18n;
+      x         >>= 27n;
+      x          %= 2n**32n;
+
+      return Number(rotr(x, count)) % 2**32;
+    }
+
+    seed(v) {
+      this.state = v + increment;
+      this.next();
+    }
+  }
+
+  Object.assign(g, { PCG_XSH_RR32 });
+})(this);
